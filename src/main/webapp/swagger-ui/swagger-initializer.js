1af4b92e6d2c90cafe64a4e20fd1143dad584b74
licontera345 <liconterapelopo@gmail.com>
2025-11-15 12:47:04 +0100

    Fix Swagger OpenAPI registration

--- src/main/webapp/swagger-ui/swagger-initializer.js
+++ src/main/webapp/swagger-ui/swagger-initializer.js
@@ -2,8 +2,14 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  const swaggerPathIndex = window.location.pathname.indexOf("/swagger-ui");
+  const basePath = swaggerPathIndex !== -1
+    ? window.location.pathname.substring(0, swaggerPathIndex)
+    : window.location.pathname.replace(/\/?$/, "");
+  const apiUrl = `${basePath.replace(/\/?$/, "")}/api/openapi.json`;
+
   window.ui = SwaggerUIBundle({
-    url: "/rentexpress-rest-api/api/openapi.json",
+    url: apiUrl,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
