e06c3ce033547411a90280b8a384de332f17a44c
Jesús Ballestero <lordrayraylord@gmail.com>
2025-11-17 18:52:30 +0100

    I

--- src/main/java/com/pinguela/rentexpress/rest/api/RestApiApplication.java
+++ src/main/java/com/pinguela/rentexpress/rest/api/RestApiApplication.java
@@ -10,33 +10,31 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
 
-//NOTACIONES con @
-
-@OpenAPIDefinition(info = @Info(
-		title = "Rest API", 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Rest API",
         version = "1.0",
         description = "demo API",
         contact = @Contact(
-		          name = "API Support", 
-		          email = "support@restapi.local",
-                  url = "https://restapi.local"
-        		),
+            name = "API Support",
+            email = "support@restapi.local",
+            url = "https://restapi.local"
+        ),
         license = @License(
-                          name = "MIT",
-                          url = "http://localhost:8080/rentexpress-rest-api/swagger-ui/index.html")
-),
-                servers = {
-                                @Server(url = "http://localhost:8080/rentexpress-rest-api") })
+            name = "MIT",
+            url = "http://localhost:8080/rentexpress-rest-api/swagger-ui/index.html"
+        )
+    ),
+    servers = {
+        @Server(url = "http://localhost:8080/rentexpress-rest-api")
+    }
+)
 @ApplicationPath("/api")
 public class RestApiApplication extends ResourceConfig {
 
-	public RestApiApplication() {
-                packages(RestApiApplication.class.getPackage().getName());
-		register(JaxrsProviders.class); // Registra quien es el que esta serializando a JSON
-
-		// Swagger annotations to document the API
-		register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
-		// register(); // para rexistrar outras clases
-	}
-
+    public RestApiApplication() {
+        packages(RestApiApplication.class.getPackage().getName());
+        register(JaxrsProviders.class);
+        register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
+    }
 }
