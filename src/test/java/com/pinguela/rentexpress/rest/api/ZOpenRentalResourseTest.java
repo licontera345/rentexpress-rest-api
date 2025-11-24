12ecdde9b5a9b92a47be0f5eac0da73036f08b9e
Jesús Ballestero <lordrayraylord@gmail.com>
2025-11-24 19:22:21 +0100

    ya

--- src/test/java/com/pinguela/rentexpress/rest/api/ZOpenRentalResourseTest.java
+++ src/test/java/com/pinguela/rentexpress/rest/api/ZOpenRentalResourseTest.java
@@ -2,6 +2,7 @@ package com.pinguela.rentexpress.rest.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -15,7 +16,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
- 
+
 import com.pinguela.rentexpres.model.RentalDTO;
 import com.pinguela.rentexpres.model.ReservationDTO;
 import com.pinguela.rentexpres.model.Results;
@@ -28,148 +29,136 @@ import jakarta.ws.rs.core.Response;
 
 public class ZOpenRentalResourseTest extends JerseyTest {
 
-    @Mock
-    private RentalService rentalService;
+	@Mock
+	private RentalService rentalService;
 
-    private AutoCloseable mocks;
+	private AutoCloseable mocks;
 
-    @Override
-    protected Application configure() {
-        mocks = MockitoAnnotations.openMocks(this);
+	@Override
+	protected Application configure() {
+		mocks = MockitoAnnotations.openMocks(this);
 
-        ZOpenRentalResourse resource = new ZOpenRentalResourse();
-        injectMock(resource, "rentalService", rentalService);
+		ZOpenRentalResourse resource = new ZOpenRentalResourse();
+		injectMock(resource, "rentalService", rentalService);
 
-        ResourceConfig rc = new ResourceConfig();
-        rc.registerInstances(resource);
-        rc.register(JavaTimeParamConverterProvider.class);
+		ResourceConfig rc = new ResourceConfig();
+		rc.registerInstances(resource);
+		rc.register(JavaTimeParamConverterProvider.class);
 
-        return rc;
-    }
+		return rc;
+	}
 
-    @Override
-    protected org.glassfish.jersey.test.spi.TestContainerFactory getTestContainerFactory() {
-        return new GrizzlyTestContainerFactory();
-    }
+	@Override
+	protected org.glassfish.jersey.test.spi.TestContainerFactory getTestContainerFactory() {
+		return new GrizzlyTestContainerFactory();
+	}
 
-    @AfterEach
-    public void tearDownMocks() throws Exception {
-        if (mocks != null) {
-            mocks.close();
-        }
-    }
+	@AfterEach
+	public void tearDownMocks() throws Exception {
+		if (mocks != null) {
+			mocks.close();
+		}
+	}
 
-    // ---------------------------------------------------
-    // TESTS
-    // ---------------------------------------------------
+	// ---------------------------------------------------
+	// TESTS
+	// ---------------------------------------------------
 
-    @Test
-    void findByIdReturnsOk() throws Exception {
-        when(rentalService.findById(1)).thenReturn(new RentalDTO());
+	@Test
+	void findByIdReturnsOk() throws Exception {
+		when(rentalService.findById(1)).thenReturn(new RentalDTO());
 
-        Response response = target("rentals/1").request().get();
+		Response response = target("rentals/1").request().get();
 
-        assertEquals(200, response.getStatus());
-    }
+		assertEquals(200, response.getStatus());
+	}
 
+	@Test
+	void createReturnsCreated() throws Exception {
+		RentalDTO rental = new RentalDTO();
+		rental.setRentalId(2);
 
-    @Test
-    void createReturnsCreated() throws Exception {
-        RentalDTO rental = new RentalDTO();
-        rental.setRentalId(2);
+		when(rentalService.create(any(RentalDTO.class))).thenReturn(true);
+		when(rentalService.findById(2)).thenReturn(rental);
 
-        when(rentalService.create(any(RentalDTO.class))).thenReturn(true);
-        when(rentalService.findById(2)).thenReturn(rental);
+		Response response = target("rentals").request().post(Entity.json(rental));
 
-        Response response = target("rentals").request().post(Entity.json(rental));
+		assertEquals(201, response.getStatus());
+	}
 
-        assertEquals(201, response.getStatus());
-    }
+	@Test
+	void updateReturnsOk() throws Exception {
+		RentalDTO rental = new RentalDTO();
 
+		when(rentalService.update(any(RentalDTO.class))).thenReturn(true);
+		when(rentalService.findById(3)).thenReturn(rental);
 
-    @Test
-    void updateReturnsOk() throws Exception {
-        RentalDTO rental = new RentalDTO();
+		Response response = target("rentals/3").request().put(Entity.json(rental));
 
-        when(rentalService.update(any(RentalDTO.class))).thenReturn(true);
-        when(rentalService.findById(3)).thenReturn(rental);
+		assertEquals(200, response.getStatus());
+	}
 
-        Response response = target("rentals/3").request().put(Entity.json(rental));
+	@Test
+	void deleteReturnsOk() throws Exception {
+		when(rentalService.delete(4)).thenReturn(true);
 
-        assertEquals(200, response.getStatus());
-    }
+		Response response = target("rentals/4").request().delete();
 
+		assertEquals(200, response.getStatus());
+	}
 
-    @Test
-    void deleteReturnsOk() throws Exception {
-        when(rentalService.delete(4)).thenReturn(true);
+	@Test
+	void findByCriteriaReturnsOk() throws Exception {
+		@SuppressWarnings("unchecked")
+		Results<RentalDTO> results = mock(Results.class);
+		when(results.getResults()).thenReturn(Collections.singletonList(new RentalDTO()));
+		when(rentalService.findByCriteria(any())).thenReturn(results);
 
-        Response response = target("rentals/4").request().delete();
+		Response response = target("rentals/search").queryParam("rentalId", 5).request().get();
 
-        assertEquals(200, response.getStatus());
-    }
+		assertEquals(200, response.getStatus());
+	}
 
+	@Test
+	void existsByReservationReturnsOk() throws Exception {
+		when(rentalService.existsByReservation(6)).thenReturn(true);
 
-    @Test
-    void findByCriteriaReturnsOk() throws Exception {
-        @SuppressWarnings("unchecked")
-        Results<RentalDTO> results = mock(Results.class);
-        when(results.getResults()).thenReturn(Collections.singletonList(new RentalDTO()));
-        when(rentalService.findByCriteria(any())).thenReturn(results);
+		Response response = target("rentals/reservations/6/exists").request().get();
 
-        Response response = target("rentals/search")
-                .queryParam("rentalId", 5)
-                .request()
-                .get();
+		assertEquals(200, response.getStatus());
+	}
 
-        assertEquals(200, response.getStatus());
-    }
+	@Test
+	void createFromReservationReturnsCreated() throws Exception {
+		ReservationDTO res = new ReservationDTO();
+		res.setReservationId(7);
 
+		doNothing().when(rentalService).createFromReservation(any(ReservationDTO.class));
+		Response response = target("rentals/from-reservation").request().post(Entity.json(res));
 
-    @Test
-    void existsByReservationReturnsOk() throws Exception {
-        when(rentalService.existsByReservation(6)).thenReturn(true);
+		assertEquals(201, response.getStatus());
+	}
 
-        Response response = target("rentals/reservations/6/exists").request().get();
+	@Test
+	void autoConvertReservationsReturnsOk() throws Exception {
+		when(rentalService.autoConvertReservations()).thenReturn(2);
 
-        assertEquals(200, response.getStatus());
-    }
+		Response response = target("rentals/auto-convert").request().post(null);
 
+		assertEquals(200, response.getStatus());
+	}
 
-    @Test
-    void createFromReservationReturnsCreated() throws Exception {
-        ReservationDTO res = new ReservationDTO();
-        res.setReservationId(7);
+	// ---------------------------------------------------
+	// HELPERS
+	// ---------------------------------------------------
 
-        when(rentalService.createFromReservation(any(ReservationDTO.class))).thenReturn(true);
-
-        Response response = target("rentals/from-reservation").request().post(Entity.json(res));
-
-        assertEquals(201, response.getStatus());
-    }
-
-
-    @Test
-    void autoConvertReservationsReturnsOk() throws Exception {
-        when(rentalService.autoConvertReservations()).thenReturn(2);
-
-        Response response = target("rentals/auto-convert").request().post(null);
-
-        assertEquals(200, response.getStatus());
-    }
-
-
-    // ---------------------------------------------------
-    // HELPERS
-    // ---------------------------------------------------
-
-    private void injectMock(Object target, String fieldName, Object value) {
-        try {
-            Field field = target.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(target, value);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private void injectMock(Object target, String fieldName, Object value) {
+		try {
+			Field field = target.getClass().getDeclaredField(fieldName);
+			field.setAccessible(true);
+			field.set(target, value);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- src/test/java/com/pinguela/rentexpress/rest/api/ZOpenVehicleResourseTest.java
+++ src/test/java/com/pinguela/rentexpress/rest/api/ZOpenVehicleResourseTest.java
@@ -14,7 +14,6 @@ import org.glassfish.jersey.test.grizzly.GrizzlyTestContainerFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.pinguela.rentexpres.model.Results;
@@ -24,112 +23,104 @@ import com.pinguela.rentexpress.rest.api.support.JavaTimeParamConverterProvider;
 
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 public class ZOpenVehicleResourseTest extends JerseyTest {
 
-    @Mock
-    private VehicleService vehicleService;
+	@Mock
+	private VehicleService vehicleService;
 
-    private AutoCloseable mocks;
+	private AutoCloseable mocks;
 
-    @Override
-    protected Application configure() {
-        mocks = MockitoAnnotations.openMocks(this);
+	@Override
+	protected Application configure() {
+		mocks = MockitoAnnotations.openMocks(this);
 
-        ZOpenVehicleResourse resource = new ZOpenVehicleResourse();
-        injectMock(resource, "vehicleService", vehicleService);
+		ZOpenVehicleResourse resource = new ZOpenVehicleResourse();
+		injectMock(resource, "vehicleService", vehicleService);
 
-        ResourceConfig rc = new ResourceConfig();
-        rc.registerInstances(resource);
-        rc.register(JavaTimeParamConverterProvider.class);
+		ResourceConfig rc = new ResourceConfig();
+		rc.registerInstances(resource);
+		rc.register(JavaTimeParamConverterProvider.class);
 
-        return rc;
-    }
+		return rc;
+	}
 
-    @Override
-    protected org.glassfish.jersey.test.spi.TestContainerFactory getTestContainerFactory() {
-        return new GrizzlyTestContainerFactory();
-    }
+	@Override
+	protected org.glassfish.jersey.test.spi.TestContainerFactory getTestContainerFactory() {
+		return new GrizzlyTestContainerFactory();
+	}
 
-    @AfterEach
-    public void tearDownMocks() throws Exception {
-        if (mocks != null) {
-            mocks.close();
-        }
-    }
+	@AfterEach
+	public void tearDownMocks() throws Exception {
+		if (mocks != null) {
+			mocks.close();
+		}
+	}
 
-    @Test
-    void findByIdReturnsOk() throws Exception {
-        when(vehicleService.findById(1)).thenReturn(new VehicleDTO());
+	@Test
+	void findByIdReturnsOk() throws Exception {
+		when(vehicleService.findById(1)).thenReturn(new VehicleDTO());
 
-        Response response = target("vehicles/1").request().get();
+		Response response = target("vehicles/1").request().get();
 
-        assertEquals(200, response.getStatus());
-    }
+		assertEquals(200, response.getStatus());
+	}
 
-    @Test
-    void createReturnsCreated() throws Exception {
-        VehicleDTO vehicle = new VehicleDTO();
-        vehicle.setVehicleId(2);
+	@Test
+	void createReturnsCreated() throws Exception {
+		VehicleDTO vehicle = new VehicleDTO();
+		vehicle.setVehicleId(2);
 
-        when(vehicleService.create(any(VehicleDTO.class))).thenReturn(true);
-        when(vehicleService.findById(2)).thenReturn(vehicle);
+		when(vehicleService.create(any(VehicleDTO.class))).thenReturn(true);
+		when(vehicleService.findById(2)).thenReturn(vehicle);
 
-        Response response = target("vehicles")
-                .request()
-                .post(Entity.json(vehicle));
+		Response response = target("vehicles").request().post(Entity.json(vehicle));
 
-        assertEquals(201, response.getStatus());
-    }
+		assertEquals(201, response.getStatus());
+	}
 
-    @Test
-    void updateReturnsOk() throws Exception {
-        VehicleDTO vehicle = new VehicleDTO();
+	@Test
+	void updateReturnsOk() throws Exception {
+		VehicleDTO vehicle = new VehicleDTO();
 
-        when(vehicleService.update(any(VehicleDTO.class))).thenReturn(true);
-        when(vehicleService.findById(3)).thenReturn(vehicle);
+		when(vehicleService.update(any(VehicleDTO.class))).thenReturn(true);
+		when(vehicleService.findById(3)).thenReturn(vehicle);
 
-        Response response = target("vehicles/3")
-                .request()
-                .put(Entity.json(vehicle));
+		Response response = target("vehicles/3").request().put(Entity.json(vehicle));
 
-        assertEquals(200, response.getStatus());
-    }
+		assertEquals(200, response.getStatus());
+	}
 
-    @Test
-    void deleteReturnsOk() throws Exception {
-        when(vehicleService.delete(4)).thenReturn(true);
+	@Test
+	void deleteReturnsOk() throws Exception {
+		when(vehicleService.delete(4)).thenReturn(true);
 
-        Response response = target("vehicles/4").request().delete();
+		Response response = target("vehicles/4").request().delete();
 
-        assertEquals(200, response.getStatus());
-    }
+		assertEquals(200, response.getStatus());
+	}
 
-    @Test
-    void findByCriteriaReturnsOk() throws Exception {
-        @SuppressWarnings("unchecked")
-        Results<VehicleDTO> results = mock(Results.class);
+	@Test
+	void findByCriteriaReturnsOk() throws Exception {
+		@SuppressWarnings("unchecked")
+		Results<VehicleDTO> results = mock(Results.class);
 
-        when(results.getResults()).thenReturn(Collections.singletonList(new VehicleDTO()));
-        when(vehicleService.findByCriteria(any())).thenReturn(results);
+		when(results.getResults()).thenReturn(Collections.singletonList(new VehicleDTO()));
+		when(vehicleService.findByCriteria(any())).thenReturn(results);
 
-        Response response = target("vehicles/search")
-                .queryParam("brand", "test")
-                .request()
-                .get();
+		Response response = target("vehicles/search").queryParam("brand", "test").request().get();
 
-        assertEquals(200, response.getStatus());
-    }
+		assertEquals(200, response.getStatus());
+	}
 
-    private void injectMock(Object target, String fieldName, Object value) {
-        try {
-            Field field = target.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(target, value);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private void injectMock(Object target, String fieldName, Object value) {
+		try {
+			Field field = target.getClass().getDeclaredField(fieldName);
+			field.setAccessible(true);
+			field.set(target, value);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
