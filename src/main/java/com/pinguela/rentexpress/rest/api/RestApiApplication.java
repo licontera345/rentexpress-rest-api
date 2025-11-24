cc24996ac811f5f2d74d13d062900f32cc2b55da
licontera345 <liconterapelopo@gmail.com>
2025-11-24 19:52:09 +0100

    Guard MultiPartFeature registration

--- src/main/java/com/pinguela/rentexpress/rest/api/RestApiApplication.java
+++ src/main/java/com/pinguela/rentexpress/rest/api/RestApiApplication.java
@@ -1,6 +1,7 @@
 package com.pinguela.rentexpress.rest.api;
 
 import org.glassfish.jersey.internal.JaxrsProviders;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.pinguela.rentexpress.rest.api.param.DateTimeJsonbProvider;
@@ -13,22 +14,24 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
 
 @OpenAPIDefinition(info = @Info(title = "Rest API",
-version = "1.0", description = "demo API", 
+version = "1.0", description = "demo API",
 contact = @Contact(name = "API Support",
 email = "support@restapi.local",
-url = "https://restapi.local"), 
-license = @License(name = "MIT", 
+url = "https://restapi.local"),
+license = @License(name = "MIT",
 url = "http://localhost:8080/rentexpress-rest-api/swagger-ui/index.html")), servers = {
-		@Server(url = "http://localhost:8080/rentexpress-rest-api") })
+                @Server(url = "http://localhost:8080/rentexpress-rest-api") })
 @ApplicationPath("/api")
 public class RestApiApplication extends ResourceConfig {
 
-	public RestApiApplication() {
-		packages(RestApiApplication.class.getPackage().getName());
-		packages("com.pinguela.rentexpress.rest.api");
-		register(JaxrsProviders.class);
-		register(DateTimeJsonbProvider.class);
-		register(org.glassfish.jersey.media.multipart.MultiPartFeature.class);
-		register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
-	}
+        public RestApiApplication() {
+                packages(RestApiApplication.class.getPackage().getName());
+                packages("com.pinguela.rentexpress.rest.api");
+                register(JaxrsProviders.class);
+                register(DateTimeJsonbProvider.class);
+                if (!isRegistered(MultiPartFeature.class)) {
+                        register(MultiPartFeature.class);
+                }
+                register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
+        }
 }
