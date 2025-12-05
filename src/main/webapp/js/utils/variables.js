ac8452f9679bc151093ad5b744910a598338aa85
Jesús Ballestero <lordrayraylord@gmail.com>
2025-12-05 12:40:51 +0100

    ya

--- src/main/webapp/js/utils/variables.js
+++ src/main/webapp/js/utils/variables.js
@@ -1,6 +1,8 @@
 const Global = {
     // Definición de la URL base de la API
-    API: "http://localhost:8080/rentexpress-rest-api/",
+    API: "http://localhost:8080/rentexpress-rest-api/api",
+	
+	
 }
 
 export default Global;
