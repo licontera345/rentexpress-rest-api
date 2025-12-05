fa9d2ab3e09e96994595cef39b8b478a42b26ead
Jesús Ballestero <lordrayraylord@gmail.com>
2025-12-05 12:23:27 +0100

    ya

---
+++ src/main/webapp/js/views/homeView.js
@@ -0,0 +1,15 @@
+const HomeView 
+
+// Contenedor HTML donde se renderizará la vista
+
+
+// Método para renderizar la vista
+
+render()
+
+
+const
+
+
+// Exporta la vista
+export default HomeView;
