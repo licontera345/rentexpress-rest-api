af565933f01242b28acc640da863d67bfa2c09e6
Jesús Ballestero <lordrayraylord@gmail.com>
2025-12-05 12:35:03 +0100

    h

--- src/main/webapp/js/utils/variables.js
+++ src/main/webapp/js/utils/variables.js
@@ -1,18 +1,6 @@
 const Global = {
-    /**
-     * URL base de la API REST.
-     * Se calcula a partir del contexto actual para evitar dependencias rígidas.
-     */
-    API: (function () {
-        var pathName = window.location.pathname;
-        var segments = pathName.split('/').filter(function (segment) {
-            return segment.length > 0;
-        });
-        if (segments.length > 0) {
-            return '/' + segments[0] + '/api';
-        }
-        return '/api';
-    })()
-};
+    // Definición de la URL base de la API
+    API: "http://localhost:8080/rentexpress-rest-api/",
+}
 
 export default Global;
