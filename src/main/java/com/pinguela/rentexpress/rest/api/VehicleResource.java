d32a28c8468c5e2afcbc5911c4a2a825166a0067
licontera345 <liconterapelopo@gmail.com>
2026-03-04 17:48:52 +0100

    d

--- src/main/java/com/pinguela/rentexpress/rest/api/VehicleResource.java
+++ src/main/java/com/pinguela/rentexpress/rest/api/VehicleResource.java
@@ -352,6 +352,7 @@ public class VehicleResource {
         if (availableFrom != null && availableTo != null) {
             criteria.setAvailableFrom(availableFrom);
             criteria.setAvailableTo(availableTo);
+            
         }
         try {
             String cacheKey = "vehicles:"
